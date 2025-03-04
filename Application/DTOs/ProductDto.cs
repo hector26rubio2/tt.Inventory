64dbe7de4148cf764b1e6c73a57acdab7963c642
hector <hector26rubio@gmail.com>
2025-03-04 17:06:00 -0500

    base for consult product

---
+++ Application/DTOs/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.DTOs
+{
+    public class ProductDto
+    {
+ 
+        public Guid Id { get; set; }
+
+ 
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Precio del producto.
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}
---
+++ Application/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿
+using Application.DTOs;
+using Transport.Queries;
+
+namespace Application.Queries
+{
+    public class GetProductByIdQuery : IQuery<ProductDto>
+    {
+        public Guid ProductId { get; set; }
+
+        public GetProductByIdQuery(Guid productId)
+        {
+            ProductId = productId;
+        }
+    }
+}
